5156ab6be4b87f4b59777326db67dccf84168288
motosw3600 <motosw3600@gmail.com>
2021-11-02 14:53:32 +0900

    [feat] Presentation Scene 추가

---
+++ Routinus/Routinus/Presentation/MyPage/MyPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageViewController.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/02.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Routinus/Routinus/Presentation/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/02.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
