2b814253a40622532f90f28ec9c8342d73c5a0ab
youseokhwan <youseokhwan15@gmail.com>
2021-11-02 16:25:05 +0900

    [feat] RoutinusNetwork 프레임워크 생성

---
+++ Routinus/RoutinusNetwork/RoutinusNetwork.swift
@@ -0,0 +1,14 @@
+//
+//  RoutinusNetwork.swift
+//  RoutinusNetwork
+//
+//  Created by 유석환 on 2021/11/02.
+//
+
+import Foundation
+
+public enum RoutinusNetwork {
+    public static func hello() -> String {
+        return "hello"
+    }
+}
