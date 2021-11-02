88e12a3e60b3f3cf7cf1095f2499c87bda4fd13d
Mingso <mpolicest@g.skku.edu>
2021-11-02 14:54:22 +0900

    [feat] 연속 달성 View 추가

--- Routinus/Routinus/Presentation/Home/HomeViewController.swift
+++ Routinus/Routinus/Presentation/Home/HomeViewController.swift
@@ -6,12 +6,80 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
+
+    private lazy var continuityView: UIView = {
+        let view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 5
+
+        return view
+    }()
+
+    private lazy var seedImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+
+        return imageView
+    }()
+
+    private lazy var continuityDayLabel: UILabel = {
+        let label = UILabel()
+
+        label.text = "0"
+        label.font = UIFont.boldSystemFont(ofSize: 48)
+        return label
+    }()
+
+    private lazy var continuityInfoLabel: UILabel = {
+        let label = UILabel()
+
+        label.text = "일 연속 달성"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        configureViews()
     }
 
+}
+
+extension HomeViewController {
+    func configureViews() {
+        self.view.backgroundColor = .white
+
+        self.view.addSubview(continuityView)
+        self.continuityView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-30)
+
+            make.top.equalToSuperview().offset(30)
+            make.height.equalTo(80)
+        }
+
+        self.continuityView.addSubview(seedImage)
+        self.seedImage.snp.makeConstraints { make in
+            make.width.height.equalTo(60)
+            make.leading.equalToSuperview().offset(20)
+            make.centerY.equalToSuperview()
+        }
+
+        self.continuityView.addSubview(continuityDayLabel)
+        self.continuityDayLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.seedImage.snp.trailing).offset(20)
+            make.centerY.equalToSuperview()
+        }
+
+        self.continuityView.addSubview(continuityInfoLabel)
+        self.continuityInfoLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.continuityDayLabel.snp.trailing).offset(5)
+            make.lastBaseline.equalTo(self.continuityDayLabel.snp.lastBaseline).offset(-2)
+        }
+    }
 }
