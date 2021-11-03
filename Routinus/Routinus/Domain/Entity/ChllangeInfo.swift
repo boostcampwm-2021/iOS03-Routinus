68a23ab0c8e0b683315fddc0a876c91289edd706
motosw3600 <motosw3600@gmail.com>
2021-11-03 17:53:07 +0900

    [feat] Domain 추가

---
+++ Routinus/Routinus/Domain/Entity/ChllangeInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ChllangeInfo.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/03.
+//
+
+import Foundation
+
+struct ChallengeInfo {
+    let identifier: String
+    let title: String
+    let joinDate: String
+    let progress: Float
+    let endDate: String
+}
