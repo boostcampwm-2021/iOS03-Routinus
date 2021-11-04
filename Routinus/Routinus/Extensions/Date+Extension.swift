890577c329befb1178335c81cbd5b39bcfc0d7ea
youseokhwan <youseokhwan15@gmail.com>
2021-11-04 12:02:00 +0900

    [feat] 일수 계산 extension 구현

---
+++ Routinus/Routinus/Extensions/Date+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Date+Extension.swift
+//  Routinus
+//
+//  Created by 유석환 on 2021/11/04.
+//
+
+import Foundation
+
+extension Date {
+    static func days(from: String, to: String) -> Int {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        
+        guard let fromDate = formatter.date(from: from),
+              let toDate = formatter.date(from: to) else { return -1 }
+        
+        let dateComponents = Calendar.current.dateComponents([.day], from: fromDate, to: toDate)
+        let days = dateComponents.day ?? -1
+        
+        return days
+    }
+}
