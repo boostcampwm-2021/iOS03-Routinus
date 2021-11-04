62a0b8b41dbc822fcf083d03e35df2930f24e2a7
motosw3600 <motosw3600@gmail.com>
2021-11-04 12:14:53 +0900

    [fix] 날짜 계산 로직 수정

--- Routinus/Routinus/Extensions/Date+Extension.swift
+++ Routinus/Routinus/Extensions/Date+Extension.swift
@@ -11,13 +11,13 @@ extension Date {
     static func days(from: String, to: String) -> Int {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyyMMdd"
-        
+
         guard let fromDate = formatter.date(from: from),
               let toDate = formatter.date(from: to) else { return -1 }
         
         let dateComponents = Calendar.current.dateComponents([.day], from: fromDate, to: toDate)
-        let days = dateComponents.day ?? -1
-        
-        return days
+        guard let days = dateComponents.day else { return -1 }
+
+        return abs(days) + 1
     }
 }
