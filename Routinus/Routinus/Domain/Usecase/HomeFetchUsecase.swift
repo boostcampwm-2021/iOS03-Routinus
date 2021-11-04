72a662f4fbbcb9e6f937ff4e98ac6f8d4b668105
motosw3600 <motosw3600@gmail.com>
2021-11-04 13:56:28 +0900

    [feat] fetchAcheievementInfo 추가

--- Routinus/Routinus/Domain/Usecase/HomeFetchUsecase.swift
+++ Routinus/Routinus/Domain/Usecase/HomeFetchUsecase.swift
@@ -12,21 +12,21 @@ import Combine
 protocol HomeFetchableUsecase {
     func fetchUserInfo()
     func fetchTodayRoutine()
-    func archeivementInfo()
+    func fetchAcheivementInfo(yearMonth: String)
 
     var userInfoSignal: PassthroughSubject<UserInfo, Never> { get }
     var todayRoutineSignal: PassthroughSubject<[TodayRoutine], Never> { get }
-    var archievementSignal: PassthroughSubject<[ArchievementInfo], Never> { get }
+    var achievementSignal: PassthroughSubject<[AchievementInfo], Never> { get }
 }
 
 struct HomeFetchUsecase: HomeFetchableUsecase {
     var userInfoSignal = PassthroughSubject<UserInfo, Never>()
     var todayRoutineSignal = PassthroughSubject<[TodayRoutine], Never>()
-    var archievementSignal = PassthroughSubject<[ArchievementInfo], Never>()
+    var achievementSignal = PassthroughSubject<[AchievementInfo], Never>()
 
     func fetchUserInfo() {
         let udid = "BD96E9E9-C0D7-46E6-BDC2-A18705B6E52C"
-        
+
         Task {
             guard let userDTO = try? await RoutinusNetwork.user(of: udid) else { return }
             let userInfo = UserInfo(userDTO: userDTO)
@@ -36,7 +36,7 @@ struct HomeFetchUsecase: HomeFetchableUsecase {
 
     func fetchTodayRoutine() {
         let udid = "BD96E9E9-C0D7-46E6-BDC2-A18705B6E52C"
-        
+
         Task {
             guard let list = try? await RoutinusNetwork.routineList(of: udid) else { return }
             let todayRoutine = list.map { TodayRoutine(todayRoutineDTO: $0) }
@@ -44,7 +44,13 @@ struct HomeFetchUsecase: HomeFetchableUsecase {
         }
     }
 
-    func archeivementInfo() {
-        
+    func fetchAcheivementInfo(yearMonth: String) {
+        let udid = "BD96E9E9-C0D7-46E6-BDC2-A18705B6E52C"
+
+        Task {
+            guard let list = try? await RoutinusNetwork.achievementInfo(of: udid, in: yearMonth) else { return }
+            let achievementInfo = list.map { AchievementInfo(achievementDTO: $0) }
+            achievementSignal.send(achievementInfo)
+        }
     }
 }
