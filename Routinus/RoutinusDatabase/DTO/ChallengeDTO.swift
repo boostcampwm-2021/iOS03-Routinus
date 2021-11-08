8d97732a124ab1a24a3cb96d0861962841e8b83e
motosw3600 <motosw3600@gmail.com>
2021-11-09 00:25:29 +0900

    [feat] ChallengeDTO 추가

---
+++ Routinus/RoutinusDatabase/DTO/ChallengeDTO.swift
@@ -0,0 +1,58 @@
+//
+//  ChallengeDTO.swift
+//  RoutinusDatabase
+//
+//  Created by 박상우 on 2021/11/09.
+//
+
+import Foundation
+
+public struct ChallengeDTO {
+    public var id: String
+    public var title: String
+    public var imageURL: String
+    public var authExampleImageURL: String
+    public var authMethod: String
+    public var categoryID: String
+    public var week: Int
+    public var decs: String
+    public var startDate: String
+    public var endDate: String
+    public var participantCount: Int
+    public var ownerID: String
+    public var thumbnailImageURL: String
+
+    public init() {
+        self.id = ""
+        self.title = ""
+        self.imageURL = ""
+        self.authExampleImageURL = ""
+        self.authMethod = ""
+        self.categoryID = ""
+        self.week = 0
+        self.decs = ""
+        self.startDate = ""
+        self.endDate = ""
+        self.participantCount = 0
+        self.ownerID = ""
+        self.thumbnailImageURL = ""
+    }
+
+    public init(id: String, title: String, imageURL: String, authExampleImageURL: String,
+                authMethod: String, categoryID: String, week: Int, decs: String, startDate: String,
+                endDate: String, participantCount: Int, ownerID: String, thumbnailImageURL: String) {
+        self.id = id
+        self.title = title
+        self.imageURL = imageURL
+        self.authExampleImageURL = authExampleImageURL
+        self.authMethod = authMethod
+        self.categoryID = categoryID
+        self.week = week
+        self.decs = decs
+        self.startDate = startDate
+        self.endDate = endDate
+        self.participantCount = participantCount
+        self.ownerID = ownerID
+        self.thumbnailImageURL = thumbnailImageURL
+    }
+}
