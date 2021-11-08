7fbf2ac95b27c368043a4308e3d295170a919116
motosw3600 <motosw3600@gmail.com>
2021-11-08 17:26:24 +0900

    [feat] ChallengeViewModel 추가

---
+++ Routinus/Routinus/Presentation/Challenge/ChallengeViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  ChallengeViewModel.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/08.
+//
+
+import Combine
+import Foundation
+
+protocol ChallengeViewModelInput {
+    func didTappedSearchButton()
+    func didTappedSeeAllButton()
+    func didTappedPopularChallenge(index: Int)
+    func didTappedCategoryButton(category: Category)
+}
+
+protocol ChallengeViewModelOutput {
+    var popularChallenge: CurrentValueSubject<[PopularChallenge], Never> { get }
+    
+    var showChallengeSearchSignal: PassthroughSubject<Void, Never> { get }
+    var showChallengeDetailSignal: PassthroughSubject<String, Never> { get }
+    var showChallengeCategorySignal: PassthroughSubject<Category, Never> { get }
+}
+
+protocol ChallengeViewModelIO: ChallengeViewModelInput, ChallengeViewModelOutput { }
+
+class ChallengeViewModel: ChallengeViewModelIO {
+    var popularChallenge = CurrentValueSubject<[PopularChallenge], Never>([])
+    
+    var showChallengeSearchSignal = PassthroughSubject<Void, Never>()
+    var showChallengeDetailSignal = PassthroughSubject<String, Never>()
+    var showChallengeCategorySignal = PassthroughSubject<Category, Never>()
+    
+    init() {
+        self.fetchChallenge()
+    }
+}
+
+extension ChallengeViewModel {
+    func didTappedSearchButton() {
+        showChallengeSearchSignal.send()
+    }
+    
+    func didTappedSeeAllButton() {
+        showChallengeSearchSignal.send()
+    }
+    
+    func didTappedPopularChallenge(index: Int) {
+        let challengeID = self.popularChallenge.value[index].challengeID
+        showChallengeDetailSignal.send(challengeID)
+    }
+    
+    func didTappedCategoryButton(category: Category) {
+        showChallengeCategorySignal.send(category)
+    }
+}
+
+extension ChallengeViewModel {
+    private func fetchChallenge() {
+        
+    }
+}
