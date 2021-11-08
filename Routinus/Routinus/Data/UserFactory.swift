43c3e75cc0a88951df10dc163ee849af79e923cd
bhyun <nkmine100@gmail.com>
2021-11-08 16:25:10 +0900

    [feat] UserFactory 분리

---
+++ Routinus/Routinus/Data/UserFactory.swift
@@ -0,0 +1,19 @@
+//
+//  UserFactory.swift
+//  Routinus
+//
+//  Created by 백지현 on 2021/11/08.
+//
+
+import Foundation
+
+class UserFactory {
+
+    static let shared = UserFactory()
+
+    private init() { }
+
+    func createRandomNickname() -> String {
+        // TODO:- 랜덤 닉네임 작업
+    }
+}
