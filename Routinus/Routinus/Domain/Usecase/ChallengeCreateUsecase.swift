702ac7c8b5488dc6f9d87db4ca3032833c685c25
bhyun <nkmine100@gmail.com>
2021-11-09 23:09:01 +0900

    [feat] 챌린지id, startDate, endDate 설정

--- Routinus/Routinus/Domain/Usecase/ChallengeCreateUsecase.swift
+++ Routinus/Routinus/Domain/Usecase/ChallengeCreateUsecase.swift
@@ -20,15 +20,37 @@ struct ChallengeCreateUsecase: ChallengeCreatableUsecase {
         self.repository = repository
     }
 
+    private func createChallengeID() -> String {
+        return UUID().uuidString
+    }
+
+    private func startDate() -> String {
+        let now = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        return dateFormatter.string(from: now)
+    }
+
+    private func endDate(week: Int) -> String? {
+        let now = Date()
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+
+        let day = DateComponents(day: week*7)
+        guard let endDate = calendar.date(byAdding: day, to: now) else { return nil }
+        return dateFormatter.string(from: endDate)
+    }
+
+
     func createChallenge(challenge: CreateChallenge) {
-        // TODO: 챌린지ID factory
-        // TODO: startDate 설정
         // TODO: categoryID 설정
-        // TODO: endDate 설정
-        guard let ownerID = repository.userID() else { return }
-        let challenge = ChallengeDTO(id: "aa", title: challenge.title, imageURL: challenge.imageURL, authExampleImageURL: challenge.authImageURL,
+        guard let ownerID = repository.userID(), let endDate = endDate(week: challenge.week) else { return }
+        let challengeID = createChallengeID()
+        let startDate = startDate()
+        let challenge = ChallengeDTO(id: challengeID, title: challenge.title, imageURL: challenge.imageURL, authExampleImageURL: challenge.authImageURL,
                                      authMethod: challenge.authMethod, categoryID: "1", week: challenge.week, decs: challenge.description,
-                                     startDate: "20211109", endDate: "20211209", participantCount: 0, ownerID: ownerID, thumbnailImageURL: "")
+                                     startDate: startDate, endDate: endDate, participantCount: 0, ownerID: ownerID, thumbnailImageURL: "")
         repository.save(challenge: challenge)
     }
 }
