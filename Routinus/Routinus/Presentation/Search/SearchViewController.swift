e5e14b5ab68f3329d1ae9bd5efc9b48c1c4e1aef
motosw3600 <motosw3600@gmail.com>
2021-11-09 23:15:13 +0900

    [feat] SearchViewController 설정

--- Routinus/Routinus/Presentation/Search/SearchViewController.swift
+++ Routinus/Routinus/Presentation/Search/SearchViewController.swift
@@ -5,25 +5,136 @@
 //  Created by 박상우 on 2021/11/02.
 //
 
+import Combine
 import UIKit
 
 class SearchViewController: UIViewController {
+    enum Section: CaseIterable {
+        case popularSearchTerm, challenge
+
+        var title: String {
+            switch self {
+            case .popularSearchTerm:
+                return "인기 검색어"
+            case .challenge:
+                return "챌린지 목록"
+            }
+        }
+    }
+
+    enum SearchContents: Hashable {
+        case popularSearchTerm(String)
+        case challenge(Challenge)
+    }
+
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, SearchContents>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, SearchContents>
+
+    private lazy var dataSource = configureDataSource()
+    private lazy var snapshot = Snapshot()
+//    private var viewModel
+    private var cancellables = Set<AnyCancellable>()
+
+    private var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .systemBackground
+
+        collectionView.showsVerticalScrollIndicator = false
+
+        collectionView.register(SearchCollectionViewHeader.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: SearchCollectionViewHeader.identifier)
+        collectionView.register(SearchPopularTermCell.self,
+                                forCellWithReuseIdentifier: SearchPopularTermCell.identifier)
+        collectionView.register(SearchChallengeCell.self,
+                                forCellWithReuseIdentifier: SearchChallengeCell.identifier)
+
+        return collectionView
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = dataSource
+        self.configureViews()
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension SearchViewController {
+    private func configureDataSource() -> DataSource {
+        let dataSource = DataSource(collectionView: self.collectionView) { collectionView, indexPath, content in
+            switch content {
+            case .popularSearchTerm(let searchTerm):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchPopularTermCell.identifier,
+                                                              for: indexPath) as? SearchPopularTermCell
+                cell?.configureViews(searchTerm: searchTerm)
+                return cell
+
+            case .challenge(let challenge):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchChallengeCell.identifier,
+                                                              for: indexPath) as? SearchChallengeCell
+                cell?.configureViews(challenge: challenge)
+                return cell
+            }
+        }
+        configureHeader(of: dataSource)
+        return dataSource
+    }
+
+    private func configureHeader(of dataSource: DataSource) {
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader else {
+                return nil
+            }
+
+            let view = collectionView.dequeueReusableSupplementaryView(
+                        ofKind: kind,
+                        withReuseIdentifier: SearchCollectionViewHeader.identifier,
+                        for: indexPath) as? SearchCollectionViewHeader
+            let section = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+
+            view?.title = section.title
+
+            switch section {
+            case .popularSearchTerm:
+                view?.configureViews()
+            case .challenge:
+                view?.configureViews()
+            }
+            return view
+        }
+    }
+
+    private func configureViews() {
+        self.view.backgroundColor = .systemBackground
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+        self.view.addSubview(collectionView)
+        self.collectionView.snp.makeConstraints { make in
+            make.leading.top.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, _) -> NSCollectionLayoutSection? in
+            let layout = SearchCollectionViewLayouts()
+            return layout.section(at: sectionNumber)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func viewTests() {
+        let popularTermItem = ["운동", "독서", "책읽기"]
     }
-    */
+}
 
+extension SearchViewController: UICollectionViewDelegate {
 }
