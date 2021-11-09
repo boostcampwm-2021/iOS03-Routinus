32c8635d11cb3f10b715edfda23a77f1056c8b6d
motosw3600 <motosw3600@gmail.com>
2021-11-09 23:14:59 +0900

    [feat] SearchCell 추가

---
+++ Routinus/Routinus/Presentation/Search/cell/SearchPopularTermCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchPopularTermCell.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/09.
+//
+
+import UIKit
+import SnapKit
+
+class SearchPopularTermCell: UICollectionViewCell {
+    static let identifier = "SearchPopularTermCell"
+
+    private var popularTerm: UIButton = {
+        let button = UIButton()
+        button.setTitle("", for: .normal)
+        button.isEnabled = false
+        button.backgroundColor = UIColor(named: "SearchTermColor")
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 15
+        return button
+    }()
+
+    func configureViews(searchTerm: String) {
+        self.popularTerm.setTitle("searchTerm", for: .normal)
+        self.addSubview(popularTerm)
+        self.popularTerm.snp.makeConstraints { make in
+            make.leading.top.trailing.bottom.equalToSuperview()
+        }
+    }
+}
