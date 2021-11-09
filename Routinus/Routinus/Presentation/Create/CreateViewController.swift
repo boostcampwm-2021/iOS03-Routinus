7d876ddbef76647c8faa37fa1fc8a7e4afbd404a
youseokhwan <youseokhwan15@gmail.com>
2021-11-09 13:26:31 +0900

    [feat] 챌린지 추가 화면으로 이동하기 위한 임시 로직 구현

---
+++ Routinus/Routinus/Presentation/Create/CreateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreateViewController.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/02.
+//
+
+import UIKit
+
+import SnapKit
+
+final class CreateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
