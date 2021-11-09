d7257d49ad44d5a0efd7ae706d1503af5a1365cc
youseokhwan <youseokhwan15@gmail.com>
2021-11-09 14:27:47 +0900

    [feat] CreateCategoryView 구현

--- Routinus/Routinus/Presentation/Create/CreateViewController.swift
+++ Routinus/Routinus/Presentation/Create/CreateViewController.swift
@@ -18,7 +18,7 @@ final class CreateViewController: UIViewController {
         stackView.spacing = 10
         return stackView
     }()
-    
+
     private lazy var categoryView = CreateCategoryView()
     private lazy var titleView = CreateTitleView()
     private lazy var imageRegisterView = CreateImageRegisterView()
@@ -32,45 +32,51 @@ final class CreateViewController: UIViewController {
         button.setTitleColor(.systemGreen, for: .normal)
         return button
     }()
-    
+
     // TODO: 임시 생성자 (CreateViewModel 작업 후 삭제)
     init() {
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     // TODO: CreateViewModel 작업 후 주석 해제
 //    init(with viewModel: CreateViewModelIO) {
 //        self.viewModel = viewModel
 //        super.init(nibName: nil, bundle: nil)
 //    }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         configureViews()
         configureViewModel()
+        configureDelegates()
     }
 }
 
 extension CreateViewController {
     private func configureViews() {
         view.backgroundColor = .white
-        
+
         view.addSubview(scrollView)
         scrollView.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide)
         }
-        
+
         scrollView.addSubview(stackView)
         stackView.snp.makeConstraints { make in
-            make.width.centerX.top.bottom.equalToSuperview()
+            make.top.bottom.left.right.equalToSuperview().inset(20)
+            make.centerX.equalToSuperview()
+        }
+
+        stackView.addArrangedSubview(categoryView)
+        categoryView.snp.makeConstraints { make in
+            make.height.equalTo(240)
         }
         
-        stackView.addArrangedSubview(categoryView)
         stackView.addArrangedSubview(titleView)
         stackView.addArrangedSubview(imageRegisterView)
         stackView.addArrangedSubview(weekView)
@@ -79,8 +85,32 @@ extension CreateViewController {
         stackView.addArrangedSubview(authImageRegisterView)
         stackView.addArrangedSubview(createButton)
     }
-    
+
     private func configureViewModel() {
         // TODO: ViewModel 작업 후 Bind
+    }
+
+    private func configureDelegates() {
+        categoryView.delegate = self
+        categoryView.dataSource = self
+    }
+}
+
+extension CreateViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Category.allCases.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Category.allCases[row].title
+    }
+    
+    // TODO: 테스트용 임시 메소드(추후 삭제)
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(Category.allCases[row].title)
     }
 }
--- Routinus/Routinus/Presentation/Create/Subviews/CreateCategory/CreateCategoryView.swift
+++ Routinus/Routinus/Presentation/Create/Subviews/CreateCategory/CreateCategoryView.swift
@@ -7,17 +7,40 @@
 
 import UIKit
 
+import SnapKit
+
 final class CreateCategoryView: UIView {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "어떤 주제와 관련이 있나요?"
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    private lazy var categoryPickerView = UIPickerView()
+    
+    weak var delegate: UIPickerViewDelegate? {
+        didSet {
+            categoryPickerView.delegate = delegate
+        }
+    }
+    
+    weak var dataSource: UIPickerViewDataSource? {
+        didSet {
+            categoryPickerView.delegate = delegate
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configure()
     }
-    
+
     convenience init() {
         self.init(frame: CGRect.zero)
     }
@@ -25,6 +48,19 @@ final class CreateCategoryView: UIView {
 
 extension CreateCategoryView {
     private func configure() {
-        
+        configureSubviews()
+    }
+
+    private func configureSubviews() {
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.left.equalToSuperview()
+        }
+
+        addSubview(categoryPickerView)
+        categoryPickerView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.left.equalToSuperview()
+        }
     }
 }
