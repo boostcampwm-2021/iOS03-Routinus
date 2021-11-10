6a545d970af411139bcf5d457a84d2e57497737f
Mingso <mpolicest@g.skku.edu>
2021-11-10 11:43:25 +0900

    [feat] 검색화면 VM, Usecase, Repo 작업

---
+++ Routinus/Routinus/Presentation/Search/SearchViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  SearchViewModel.swift
+//  Routinus
+//
+//  Created by 김민서 on 2021/11/09.
+//
+
+import Combine
+import Foundation
+
+protocol SearchViewModelInput {
+    func didTappedSearchButton(keyword: String)
+    func didTappedChallenge(index: Int)
+}
+
+protocol SearchViewModelOutput {
+    var latestChallenge: CurrentValueSubject<[Challenge], Never> { get }
+
+    var showChallengeSearchSignal: PassthroughSubject<String, Never> { get }
+    var showChallengeDetailSignal: PassthroughSubject<String, Never> { get }
+    var showChallengeCategorySignal: PassthroughSubject<Category, Never> { get }
+}
+
+protocol SearchViewModelIO: SearchViewModelInput, SearchViewModelOutput { }
+
+class SearchViewModel: SearchViewModelIO {
+    var latestChallenge = CurrentValueSubject<[Challenge], Never>([])
+
+    var showChallengeSearchSignal = PassthroughSubject<String, Never>()
+    var showChallengeDetailSignal = PassthroughSubject<String, Never>()
+    var showChallengeCategorySignal = PassthroughSubject<Category, Never>()
+
+    let usecase: SearchFetchableUsecase
+    var cancellables = Set<AnyCancellable>()
+
+    init(usecase: SearchFetchableUsecase) {
+        self.usecase = usecase
+        self.fetchChallenge()
+    }
+}
+
+extension SearchViewModel {
+    func didTappedSearchButton(keyword: String) {
+        showChallengeSearchSignal.send(keyword)
+    }
+
+    func didTappedChallenge(index: Int) {
+        let challengeID = self.latestChallenge.value[index].challengeID
+        showChallengeDetailSignal.send(challengeID)
+    }
+}
+
+extension SearchViewModel {
+    private func fetchChallenge() {
+        usecase.fetchPopularChallenge { [weak self] challenge in
+            self?.latestChallenge.value = challenge
+        }
+    }
+}
--- Routinus/RoutinusDatabase/RoutinusDatabase.swift
+++ Routinus/RoutinusDatabase/RoutinusDatabase.swift
@@ -159,4 +159,33 @@ public enum RoutinusDatabase {
 
         return challengeList
     }
+
+    public static func allChallenges() async throws -> [ChallengeDTO] {
+        let db = Firestore.firestore()
+        let snapshot = try await db.collection("challenge")
+            .order(by: "participant_count", descending: true)
+            .getDocuments()
+
+        var challengeList = [ChallengeDTO]()
+
+        for document in snapshot.documents {
+            let challengeDTO = ChallengeDTO(
+                id: document["id"] as? String ?? "",
+                title: document["title"] as? String ?? "",
+                imageURL: document["image_url"] as? String ?? "",
+                authExampleImageURL: document["auth_example_image_url"] as? String ?? "",
+                authMethod: document["auth_method"] as? String ?? "",
+                categoryID: document["category_id"] as? String ?? "",
+                week: document["week"] as? Int ?? 0,
+                decs: document["desc"] as? String ?? "",
+                startDate: document["start_date"] as? String ?? "",
+                endDate: document["end_date"] as? String ?? "",
+                participantCount: document["participant_count"] as? Int ?? 0,
+                ownerID: document["owner_id"] as? String ?? "",
+                thumbnailImageURL: document["thumbnail_image_url"] as? String ?? ""
+            )
+            challengeList.append(challengeDTO)
+        }
+        return challengeList
+    }
 }
