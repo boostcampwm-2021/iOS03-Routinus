34e197c304bd6a7d4f4f7f7abc5a606cae6ff38d
youseokhwan <youseokhwan15@gmail.com>
2021-11-10 23:22:36 +0900

    [feat] CreateImagePickerDelegate 프로토콜 정의

---
+++ Routinus/Routinus/Presentation/Create/Delegates/CreateImagePickerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CreateImagePickerDelegate.swift
+//  Routinus
+//
+//  Created by 유석환 on 2021/11/10.
+//
+
+import Foundation
+
+protocol CreateImagePickerDelegate: AnyObject {
+    func didTappedImageView()
+}
