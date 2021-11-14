6465ee77563b664db00bd3c5480574d6fc0e8b37
youseokhwan <youseokhwan15@gmail.com>
2021-11-14 17:02:27 +0900

    [refactor] Firebase SDK 제거, Query 파일 분리

--- Routinus/RoutinusDatabase/RoutinusDatabase.swift
+++ Routinus/RoutinusDatabase/RoutinusDatabase.swift
@@ -7,15 +7,9 @@
 
 import Foundation
 
-import Firebase
-
 public enum RoutinusDatabase {
     private static let firestoreURL = "https://firestore.googleapis.com/v1/projects/boostcamp-ios03-routinus/databases/(default)/documents"
     private static let storageURL = "https://firebasestorage.googleapis.com/v0/b/boostcamp-ios03-routinus.appspot.com/o"
-
-    public static func configure() {
-        FirebaseApp.configure()
-    }
 
     public static func imageURL(id: String, fileName: String) async throws -> URL? {
         return URL(string: "\(storageURL)/\(id)%2F\(fileName).jpeg?alt=media")
@@ -27,17 +21,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "fields": {
-                "id": { "stringValue": "\(id)" },
-                "name": { "stringValue": "\(name)" },
-                "grade": { "integerValue": "0" },
-                "continuity_day": { "integerValue": "0" },
-                "user_image_category_id": { "stringValue": "0" }
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.createUserQuery(id: id, name: name)
 
         _ = try await URLSession.shared.data(for: request)
     }
@@ -62,22 +46,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "fields": {
-                "auth_method": { "stringValue": "\(document.authMethod.stringValue)" },
-                "category_id": { "stringValue": "\(document.categoryID.stringValue)" },
-                "desc": { "stringValue": "\(document.desc.stringValue)" },
-                "end_date": { "stringValue": "\(document.endDate.stringValue)" },
-                "id": { "stringValue": "\(document.id.stringValue)" },
-                "owner_id": { "stringValue": "\(document.ownerID.stringValue)" },
-                "participant_count": { "integerValue": "\(document.participantCount.integerValue)" },
-                "start_date": { "stringValue": "\(document.startDate.stringValue)" },
-                "title": { "stringValue": "\(document.title.stringValue)" },
-                "week": { "integerValue": "\(document.week.integerValue)" }
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.insertChallengeQuery(document: document)
 
         _ = try await URLSession.shared.data(for: request)
     }
@@ -89,16 +58,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "fields": {
-                "auth_count": { "integerValue": "0" },
-                "challenge_id": { "stringValue": "\(document.id.stringValue)" },
-                "join_date": { "stringValue": "\(document.startDate.stringValue)" },
-                "user_id": { "stringValue": "\(document.ownerID.stringValue)" }
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.insertChallengeParticipationQuery(document: document)
 
         _ = try await URLSession.shared.data(for: request)
     }
@@ -121,20 +81,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": { "collectionId": "user" },
-                "where": {
-                    "fieldFilter": {
-                        "field": { "fieldPath": "id" },
-                        "op": "EQUAL",
-                        "value": { "stringValue": "\(id)" }
-                    },
-                },
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.userQuery(of: id)
 
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode([UserDTO].self, from: data).first ?? UserDTO()
@@ -146,20 +93,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": { "collectionId": "challenge_participation" },
-                "where": {
-                    "fieldFilter": {
-                        "field": { "fieldPath": "user_id" },
-                        "op": "EQUAL",
-                        "value": { "stringValue": "\(id)" }
-                    },
-                },
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.routinesQuery(userID: id)
 
         var (data, _) = try await URLSession.shared.data(for: request)
         let participations = try JSONDecoder().decode([ParticipationDTO].self, from: data)
@@ -168,20 +102,7 @@ public enum RoutinusDatabase {
         for participation in participations {
             guard let challengeID = participation.document?.fields.challengeID.stringValue else { continue }
 
-            request.httpBody = """
-            {
-                "structuredQuery": {
-                    "from": { "collectionId": "challenge" },
-                    "where": {
-                        "fieldFilter": {
-                            "field": { "fieldPath": "id" },
-                            "op": "EQUAL",
-                            "value": { "stringValue": "\(challengeID)" }
-                        },
-                    },
-                }
-            }
-            """.data(using: .utf8)
+            request.httpBody = RoutinusQuery.routinesQuery(challengeID: challengeID)
 
             (data, _) = try await URLSession.shared.data(for: request)
             let challenge = try JSONDecoder().decode([ChallengeDTO].self, from: data).first ?? ChallengeDTO()
@@ -198,34 +119,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": { "collectionId": "achievement" },
-                "where": {
-                    "compositeFilter": {
-                        "filters": [
-                            {
-                                "fieldFilter": {
-                                    "field": { "fieldPath": "user_id" },
-                                    "op": "EQUAL",
-                                    "value": { "stringValue": "\(id)" }
-                                }
-                            },
-                            {
-                                "fieldFilter": {
-                                    "field": { "fieldPath": "year_month" },
-                                    "op": "EQUAL",
-                                    "value": { "stringValue": "\(yearMonth)" }
-                                },
-                            }
-                        ],
-                        "op": "AND"
-                    }
-                }
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.achievementQuery(of: id, in: yearMonth)
 
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode([AchievementDTO].self, from: data)
@@ -237,22 +131,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": {
-                    "collectionId": "challenge",
-                },
-                "orderBy": [
-                    {
-                        "field": { "fieldPath": "participant_count" },
-                        "direction": "ASCENDING"
-                    },
-                ],
-                "limit": 50
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.allChallengesQuery()
 
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode([ChallengeDTO].self, from: data)
@@ -264,22 +143,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": {
-                    "collectionId": "challenge",
-                },
-                "orderBy": [
-                    {
-                        "field": { "fieldPath": "start_date" },
-                        "direction": "DESCENDING"
-                    },
-                ],
-                "limit": 10
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.newChallengeQuery()
 
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode([ChallengeDTO].self, from: data)
@@ -291,22 +155,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": {
-                    "collectionId": "challenge",
-                },
-                "orderBy": [
-                    {
-                        "field": { "fieldPath": "participant_count" },
-                        "direction": "DESCENDING"
-                    },
-                ],
-                "limit": 5
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.recommendChallengeQuery()
 
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode([ChallengeDTO].self, from: data)
@@ -318,28 +167,7 @@ public enum RoutinusDatabase {
 
         request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = """
-        {
-            "structuredQuery": {
-                "from": {
-                    "collectionId": "challenge",
-                },
-                "where": {
-                    "fieldFilter": {
-                        "field": { "fieldPath": "category_id" },
-                        "op": "EQUAL",
-                        "value": { "stringValue": "\(categoryID)" }
-                    },
-                },
-                "orderBy": [
-                    {
-                        "field": { "fieldPath": "participant_count" },
-                        "direction": "DESCENDING"
-                    },
-                ]
-            }
-        }
-        """.data(using: .utf8)
+        request.httpBody = RoutinusQuery.searchChallenges(by: categoryID)
 
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode([ChallengeDTO].self, from: data)
---
+++ Routinus/RoutinusDatabase/RoutinusQuery.swift
@@ -0,0 +1,220 @@
+//
+//  RoutinusQuery.swift
+//  RoutinusDatabase
+//
+//  Created by 유석환 on 2021/11/14.
+//
+
+import Foundation
+
+internal enum RoutinusQuery {
+    internal static func createUserQuery(id: String, name: String) -> Data? {
+        return """
+        {
+            "fields": {
+                "id": { "stringValue": "\(id)" },
+                "name": { "stringValue": "\(name)" },
+                "grade": { "integerValue": "0" },
+                "continuity_day": { "integerValue": "0" },
+                "user_image_category_id": { "stringValue": "0" }
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func insertChallengeQuery(document: ChallengeField) -> Data? {
+        return """
+        {
+            "fields": {
+                "auth_method": { "stringValue": "\(document.authMethod.stringValue)" },
+                "category_id": { "stringValue": "\(document.categoryID.stringValue)" },
+                "desc": { "stringValue": "\(document.desc.stringValue)" },
+                "end_date": { "stringValue": "\(document.endDate.stringValue)" },
+                "id": { "stringValue": "\(document.id.stringValue)" },
+                "owner_id": { "stringValue": "\(document.ownerID.stringValue)" },
+                "participant_count": { "integerValue": "\(document.participantCount.integerValue)" },
+                "start_date": { "stringValue": "\(document.startDate.stringValue)" },
+                "title": { "stringValue": "\(document.title.stringValue)" },
+                "week": { "integerValue": "\(document.week.integerValue)" }
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func insertChallengeParticipationQuery(document: ChallengeField) -> Data? {
+        return """
+        {
+            "fields": {
+                "auth_count": { "integerValue": "0" },
+                "challenge_id": { "stringValue": "\(document.id.stringValue)" },
+                "join_date": { "stringValue": "\(document.startDate.stringValue)" },
+                "user_id": { "stringValue": "\(document.ownerID.stringValue)" }
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func userQuery(of id: String) -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": { "collectionId": "user" },
+                "where": {
+                    "fieldFilter": {
+                        "field": { "fieldPath": "id" },
+                        "op": "EQUAL",
+                        "value": { "stringValue": "\(id)" }
+                    },
+                },
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func routinesQuery(userID: String) -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": { "collectionId": "challenge_participation" },
+                "where": {
+                    "fieldFilter": {
+                        "field": { "fieldPath": "user_id" },
+                        "op": "EQUAL",
+                        "value": { "stringValue": "\(userID)" }
+                    },
+                },
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func routinesQuery(challengeID: String) -> Data? {
+        return """
+            {
+                "structuredQuery": {
+                    "from": { "collectionId": "challenge" },
+                    "where": {
+                        "fieldFilter": {
+                            "field": { "fieldPath": "id" },
+                            "op": "EQUAL",
+                            "value": { "stringValue": "\(challengeID)" }
+                        },
+                    },
+                }
+            }
+            """.data(using: .utf8)
+    }
+
+    internal static func achievementQuery(of id: String, in yearMonth: String) -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": { "collectionId": "achievement" },
+                "where": {
+                    "compositeFilter": {
+                        "filters": [
+                            {
+                                "fieldFilter": {
+                                    "field": { "fieldPath": "user_id" },
+                                    "op": "EQUAL",
+                                    "value": { "stringValue": "\(id)" }
+                                }
+                            },
+                            {
+                                "fieldFilter": {
+                                    "field": { "fieldPath": "year_month" },
+                                    "op": "EQUAL",
+                                    "value": { "stringValue": "\(yearMonth)" }
+                                },
+                            }
+                        ],
+                        "op": "AND"
+                    }
+                }
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func allChallengesQuery() -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": {
+                    "collectionId": "challenge",
+                },
+                "orderBy": [
+                    {
+                        "field": { "fieldPath": "participant_count" },
+                        "direction": "ASCENDING"
+                    },
+                ],
+                "limit": 50
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func newChallengeQuery() -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": {
+                    "collectionId": "challenge",
+                },
+                "orderBy": [
+                    {
+                        "field": { "fieldPath": "start_date" },
+                        "direction": "DESCENDING"
+                    },
+                ],
+                "limit": 10
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func recommendChallengeQuery() -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": {
+                    "collectionId": "challenge",
+                },
+                "orderBy": [
+                    {
+                        "field": { "fieldPath": "participant_count" },
+                        "direction": "DESCENDING"
+                    },
+                ],
+                "limit": 5
+            }
+        }
+        """.data(using: .utf8)
+    }
+
+    internal static func searchChallenges(by categoryID: String) -> Data? {
+        return """
+        {
+            "structuredQuery": {
+                "from": {
+                    "collectionId": "challenge",
+                },
+                "where": {
+                    "fieldFilter": {
+                        "field": { "fieldPath": "category_id" },
+                        "op": "EQUAL",
+                        "value": { "stringValue": "\(categoryID)" }
+                    },
+                },
+                "orderBy": [
+                    {
+                        "field": { "fieldPath": "participant_count" },
+                        "direction": "DESCENDING"
+                    },
+                ]
+            }
+        }
+        """.data(using: .utf8)
+    }
+}
