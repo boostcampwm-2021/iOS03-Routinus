85e9b384303333bc64762d63eed608e885a21c22
youseokhwan <youseokhwan15@gmail.com>
2021-11-15 15:12:11 +0900

    [feat] RoutinusImageManager 모듈 생성

---
+++ Routinus/RoutinusImageManager/RoutinusImageManager.swift
@@ -0,0 +1,14 @@
+//
+//  RoutinusImageManager.swift
+//  RoutinusImageManager
+//
+//  Created by 유석환 on 2021/11/15.
+//
+
+import Foundation
+
+public enum RoutinusImageManager {
+    public static func hello() {
+        print("hello")
+    }
+}
