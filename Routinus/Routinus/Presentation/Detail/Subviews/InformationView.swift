6728a26575a3d688c52bfd05f353296baaa4a2ac
bhyun <nkmine100@gmail.com>
2021-11-17 17:53:49 +0900

    [chore] 불필요한 코드 삭제

--- Routinus/Routinus/Presentation/Detail/Subviews/InformationView.swift
+++ Routinus/Routinus/Presentation/Detail/Subviews/InformationView.swift
@@ -111,7 +111,6 @@ final class InformationView: UIView {
 
     private lazy var introductionLabel: UILabel = {
         var label = UILabel()
-//        label.text = "ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㄹㅇㄹㅇ ㅁㄹㅇㅍㄹㅇㄹㅁㄹㅇㄹㅁㄹㅁㅇㄹㅁㅇㄹㅁㅇㄹㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㄹㅇㄹㅇ ㅁㄹㅇㅍㄹㅇㄹㅁㄹㅇㄹㅁㄹㅁㅇㄹㅁㅇㄹㅁㅇㄹㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㄹㅇㄹㅇ ㅁㄹㅇㅍㄹㅇㄹㅁㄹㅇㄹㅁㄹㅁㅇㄹㅁㅇㄹㅁㅇㄹㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㄹㅇㄹㅇ ㅁㄹㅇㅍㄹㅇㄹㅁㄹㅇㄹㅁㄹㅁㅇㄹ"
         label.text = "꾸준히 할 수 있는 운동을 찾으시거나 다이어트 중인데 헬스장은 가기 싫으신분들 함께 1만보씩 걸어요~"
         label.numberOfLines = 0
         label.font = UIFont.boldSystemFont(ofSize: 14)
