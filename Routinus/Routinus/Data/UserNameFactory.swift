e8313708ba88919e704b19969b811d4ef536444f
youseokhwan <youseokhwan15@gmail.com>
2021-11-18 18:31:01 +0900

    [refactor] namefactory 수정

--- Routinus/Routinus/Data/UserNameFactory.swift
+++ Routinus/Routinus/Data/UserNameFactory.swift
@@ -11,8 +11,7 @@ enum UserNameFactory {
     static func createRandomName() -> String {
         var result = ""
         let adjective = ["귀여운", "멋진", "정직한", "예쁜", "스윗한", "로맨틱한", "재미있는", "느긋한", "친절한", "다정한"]
-        let noun = ["컵라면", "코끼리", "감자", "소고기", "맥북", "민서", "지현", "석환", "상우", "강아지", "고양이", "토끼",
-                    "송아지", "사자", "호랑이", "병아리"]
+        let noun = ["컵라면", "코끼리", "감자", "소고기", "맥북", "강아지", "고양이", "토끼", "송아지", "사자", "호랑이", "병아리"]
         let number = Int.random(in: 1...99)
 
         result += adjective.randomElement() ?? ""
