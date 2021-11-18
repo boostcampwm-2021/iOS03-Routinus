19ee5c2f57dcdb5a8fedc7c890bc643bbbd5afe7
youseokhwan <youseokhwan15@gmail.com>
2021-11-18 18:34:49 +0900

    [refactor] 더미 id 삭제

--- Routinus/Routinus/Domain/Usecase/AchievementFetchUsecase.swift
+++ Routinus/Routinus/Domain/Usecase/AchievementFetchUsecase.swift
@@ -21,9 +21,7 @@ struct AchievementFetchUsecase: AchievementFetchableUsecase {
 
     func fetchAchievements(yearMonth: String,
                            completion: @escaping ([Achievement]) -> Void) {
-        // TODO: 테스트를 위한 임시 id(챌린지 추가 이후 guard 구문으로 교체)
-        let id = "b555645c4804df095d82cb0b951a03b00d69cdeca5afc0a51201e1bfeae75e9b"
-//        guard let id = RoutinusRepository.userID() else { return }
+        guard let id = RoutinusRepository.userID() else { return }
 
         repository.fetchAchievements(by: id, in: yearMonth) { achievements in
             completion(achievements)
--- Routinus/Routinus/Domain/Usecase/UserFetchUsecase.swift
+++ Routinus/Routinus/Domain/Usecase/UserFetchUsecase.swift
@@ -20,9 +20,7 @@ struct UserFetchUsecase: UserFetchableUsecase {
     }
 
     func fetchUser(completion: @escaping (User) -> Void) {
-        // TODO: 테스트를 위한 임시 id(챌린지 추가 이후 guard 구문으로 교체)
-        let id = "b555645c4804df095d82cb0b951a03b00d69cdeca5afc0a51201e1bfeae75e9b"
-//        guard let id = RoutinusRepository.userID() else { return }
+        guard let id = RoutinusRepository.userID() else { return }
 
         repository.fetchUser(by: id) { user in
             completion(user)
