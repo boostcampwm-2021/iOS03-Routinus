e429e68f24b6f4dfedf96453e7f74c7f7f07d879
motosw3600 <motosw3600@gmail.com>
2021-11-20 19:44:11 +0900

    [feat] NotificationName Extension 추가

---
+++ Routinus/Routinus/Extensions/NotificationName+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+Extensions.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/20.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let completionAuth = Notification.Name("completionAuth")
+}
