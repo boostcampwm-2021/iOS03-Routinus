4ceb2b8f7f2f779826da2c89078e51609cffe950
motosw3600 <motosw3600@gmail.com>
2021-11-24 13:44:20 +0900

    [feat] ChallengeCategoryHeaderDeleagte 추가

---
+++ Routinus/Routinus/Presentation/Challenge/Delegates/ChallengeCategoryHeaderDeleagte.swift
@@ -0,0 +1,12 @@
+//
+//  ChallengeCollectionViewHeaderDeleagte.swift
+//  Routinus
+//
+//  Created by 박상우 on 2021/11/24.
+//
+
+import Foundation
+
+protocol ChallengeCategoryHeaderDeleagte: AnyObject {
+    func didTappedSeeAllButton()
+}
