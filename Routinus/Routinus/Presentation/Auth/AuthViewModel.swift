0e7574fa0b223fb41fe7a22e90c8f6d94578da0d
motosw3600 <motosw3600@gmail.com>
2021-11-25 15:48:29 +0900

    [fix] fetchChallenge 접근제어 수정

--- Routinus/Routinus/Presentation/Auth/AuthViewModel.swift
+++ Routinus/Routinus/Presentation/Auth/AuthViewModel.swift
@@ -9,6 +9,7 @@ import Combine
 import Foundation
 
 protocol AuthViewModelInput {
+    func fetchChallenge(challengeID: String)
     func didTappedAuthButton()
     func didTappedAlertConfirm()
     func didTappedAuthMethodImage(image: Data)
@@ -129,7 +130,7 @@ extension AuthViewModel {
 }
 
 extension AuthViewModel {
-    private func fetchChallenge(challengeID: String) {
+    func fetchChallenge(challengeID: String) {
         self.challengeFetchUsecase.fetchChallenge(challengeID: challengeID) { [weak self] challenge in
             guard let self = self else { return }
             self.challenge.value = challenge
