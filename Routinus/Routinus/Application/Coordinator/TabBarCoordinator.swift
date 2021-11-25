c208e3dbb13a607098fb31fa65049e8ff9d9bf9a
youseokhwan <youseokhwan15@gmail.com>
2021-11-25 17:43:57 +0900

    [feat] 탭바 아이콘 텍스트 추가

--- Routinus/Routinus/Application/Coordinator/TabBarCoordinator.swift
+++ Routinus/Routinus/Application/Coordinator/TabBarCoordinator.swift
@@ -8,6 +8,52 @@
 import UIKit
 
 final class TabBarCoordinator: NSObject, RoutinusCoordinator {
+    enum TabBarPage {
+        case challenge
+        case home
+        case manage
+        case myPage
+
+        func title() -> String {
+            switch self {
+            case .home:
+                return "Home"
+            case .challenge:
+                return "Challenge"
+            case .manage:
+                return "Manage"
+            case .myPage:
+                return "MyPage"
+            }
+        }
+
+        func tabBarIndex() -> Int {
+            switch self {
+            case .home:
+                return 0
+            case .challenge:
+                return 1
+            case .manage:
+                return 2
+            case .myPage:
+                return 3
+            }
+        }
+
+        func tabBarSelectedImage() -> UIImage? {
+            switch self {
+            case .home:
+                return UIImage(systemName: "house")
+            case .challenge:
+                return UIImage(named: "challenge")
+            case .manage:
+                return UIImage(systemName: "highlighter")
+            case .myPage:
+                return UIImage(systemName: "person")
+            }
+        }
+    }
+
     var childCoordinator: [RoutinusCoordinator] = []
     var navigationController: UINavigationController
     var tabBarController: UITabBarController
@@ -30,7 +76,7 @@ final class TabBarCoordinator: NSObject, RoutinusCoordinator {
 
     private func getTabBarController(_ page: TabBarPage) -> UINavigationController {
         let navigationController = UINavigationController()
-        navigationController.tabBarItem = UITabBarItem.init(title: nil,
+        navigationController.tabBarItem = UITabBarItem.init(title: page.title(),
                                                             image: page.tabBarSelectedImage(),
                                                             selectedImage: page.tabBarSelectedImage())
 
@@ -63,38 +109,5 @@ final class TabBarCoordinator: NSObject, RoutinusCoordinator {
         tabBarController.tabBar.backgroundColor = .systemBackground
         tabBarController.tabBar.isTranslucent = false
         navigationController.viewControllers = [tabBarController]
-    }
-}
-
-enum TabBarPage {
-    case challenge
-    case home
-    case manage
-    case myPage
-
-    func tabBarIndex() -> Int {
-        switch self {
-        case .home:
-            return 0
-        case .challenge:
-            return 1
-        case .manage:
-            return 2
-        case .myPage:
-            return 3
-        }
-    }
-
-    func tabBarSelectedImage() -> UIImage? {
-        switch self {
-        case .home:
-            return UIImage(systemName: "house")
-        case .challenge:
-            return UIImage(named: "challenge")
-        case .manage:
-            return UIImage(systemName: "highlighter")
-        case .myPage:
-            return UIImage(systemName: "person")
-        }
     }
 }
