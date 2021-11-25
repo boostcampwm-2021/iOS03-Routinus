a7df50d9959b90fe5f3e0f5f419a88a83232e188
motosw3600 <motosw3600@gmail.com>
2021-11-25 15:04:25 +0900

    [feat] 검색 화면 swipe 데이터 로딩 적용

--- Routinus/Routinus/Presentation/Search/SearchViewController.swift
+++ Routinus/Routinus/Presentation/Search/SearchViewController.swift
@@ -70,6 +70,7 @@ final class SearchViewController: UIViewController {
         self.snapshot.appendSections(Section.allCases)
         self.configureViews()
         self.configureViewModel()
+        self.configureRefreshControl()
         self.didLoadedSearchView()
     }
 }
@@ -171,6 +172,26 @@ extension SearchViewController {
         return UICollectionViewCompositionalLayout { (sectionNumber, _) -> NSCollectionLayoutSection? in
             let layout = SearchCollectionViewLayouts()
             return layout.section(at: sectionNumber)
+        }
+    }
+
+    private func configureRefreshControl() {
+        let refresh = UIRefreshControl()
+        refresh.addTarget(self,
+                           action: #selector(refreshData),
+                           for: .valueChanged)
+        refresh.attributedTitle = NSAttributedString(string: "Loading Data...",
+                                                     attributes: [NSAttributedString.Key.foregroundColor:
+                                                                    UIColor.systemGray,
+                                                                  NSAttributedString.Key.font:
+                                                                    UIFont.boldSystemFont(ofSize: 20)])
+        self.collectionView.refreshControl = refresh
+    }
+
+    @objc private func refreshData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            self.viewModel?.didLoadedSearchView()
+            self.collectionView.refreshControl?.endRefreshing()
         }
     }
 }
