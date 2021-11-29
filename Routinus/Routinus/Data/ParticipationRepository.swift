960e11b836b983098b6daceb9db141101d524ee5
bhyun <nkmine100@gmail.com>
2021-11-29 17:47:21 +0900

    [chore] ParticipationRepository 개행 수정

--- Routinus/Routinus/Data/ParticipationRepository.swift
+++ Routinus/Routinus/Data/ParticipationRepository.swift
@@ -11,21 +11,16 @@ protocol ParticipationRepository {
     func fetchChallengeParticipation(userID: String,
                                      challengeID: String,
                                      completion: @escaping (Participation?) -> Void)
-    func save(challengeID: String,
-              joinDate: String,
-              completion: (() -> Void)?)
-    func updateAuthCount(challengeID: String,
-                         completion: (() -> Void)?)
+    func save(challengeID: String, joinDate: String, completion: (() -> Void)?)
+    func updateAuthCount(challengeID: String, completion: (() -> Void)?)
 }
 
 extension RoutinusRepository: ParticipationRepository {
     func fetchChallengeParticipation(userID: String,
                                      challengeID: String,
                                      completion: @escaping (Participation?) -> Void) {
-        FirebaseService.challengeParticipation(userID: userID,
-                                               challengeID: challengeID) { dto in
-            guard let dto = dto,
-                  dto.document != nil else {
+        FirebaseService.challengeParticipation(userID: userID, challengeID: challengeID) { dto in
+            guard let dto = dto, dto.document != nil else {
                 completion(nil)
                 return
             }
@@ -33,9 +28,7 @@ extension RoutinusRepository: ParticipationRepository {
         }
     }
 
-    func save(challengeID: String,
-              joinDate: String,
-              completion: (() -> Void)?) {
+    func save(challengeID: String, joinDate: String, completion: (() -> Void)?) {
         guard let userID = RoutinusRepository.userID() else { return }
         let dto = ParticipationDTO(authCount: 0,
                                    challengeID: challengeID,
@@ -46,8 +39,7 @@ extension RoutinusRepository: ParticipationRepository {
         }
     }
 
-    func updateAuthCount(challengeID: String,
-                         completion: (() -> Void)?) {
+    func updateAuthCount(challengeID: String, completion: (() -> Void)?) {
         guard let userID = RoutinusRepository.userID() else { return }
         FirebaseService.updateChallengeParticipationAuthCount(challengeID: challengeID,
                                                               userID: userID) {
