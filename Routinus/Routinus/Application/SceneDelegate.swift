1f51d398d1284eef3c61821580e6d40ed7351106
Mingso <mpolicest@g.skku.edu>
2021-11-29 17:11:33 +0900

    [refactor] SceneDelegate 개행 수정

--- Routinus/Routinus/Application/SceneDelegate.swift
+++ Routinus/Routinus/Application/SceneDelegate.swift
@@ -11,7 +11,9 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     var appCoordinator: AppCoordinator?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let naviationController = UINavigationController()
