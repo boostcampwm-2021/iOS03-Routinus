1cc6f0c65db1624be6cb7359b72b276b6c05b8d4
motosw3600 <motosw3600@gmail.com>
2021-11-29 17:11:46 +0900

    [refactor] AppDelegate 개행 수정

--- Routinus/Routinus/Application/AppDelegate.swift
+++ Routinus/Routinus/Application/AppDelegate.swift
@@ -14,7 +14,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
