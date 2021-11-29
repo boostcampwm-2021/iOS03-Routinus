3b4f6a7e557b22c9125af3cbaf8a733fc35b87ed
Mingso <mpolicest@g.skku.edu>
2021-11-29 17:45:07 +0900

    [chore] UserRepository 개행 수정

--- Routinus/Routinus/Data/UserRepository.swift
+++ Routinus/Routinus/Data/UserRepository.swift
@@ -9,19 +9,12 @@ import Foundation
 
 protocol UserRepository {
     func isEmptyUserID() -> Bool
-    func save(id: String,
-              name: String,
-              completion: (() -> Void)?)
-    func fetchUser(by id: String,
-                   completion: ((User) -> Void)?)
-    func updateContinuityDay(by id: String,
-                             completion: ((UserDTO) -> Void)?)
-    func updateContinuityDayByAuth(by id: String,
-                                   completion: (() -> Void)?)
+    func save(id: String, name: String, completion: (() -> Void)?)
+    func fetchUser(by id: String, completion: ((User) -> Void)?)
+    func updateContinuityDay(by id: String, completion: ((UserDTO) -> Void)?)
+    func updateContinuityDayByAuth(by id: String, completion: (() -> Void)?)
     func fetchThemeStyle() -> Int
-    func updateUsername(of id: String,
-                        name: String,
-                        completion: (() -> Void)?)
+    func updateUsername(of id: String, name: String, completion: (() -> Void)?)
     func updateThemeStyle(_ style: Int)
 }
 
@@ -30,33 +23,26 @@ extension RoutinusRepository: UserRepository {
         return UserDefaults.standard.string(forKey: RoutinusRepository.userIDKey) == nil
     }
 
-    func save(id: String,
-              name: String,
-              completion: (() -> Void)?) {
-        UserDefaults.standard.set(id,
-                                  forKey: RoutinusRepository.userIDKey)
-        FirebaseService.insertUser(id: id,
-                                   name: name) {
+    func save(id: String, name: String, completion: (() -> Void)?) {
+        UserDefaults.standard.set(id, forKey: RoutinusRepository.userIDKey)
+        FirebaseService.insertUser(id: id, name: name) {
             completion?()
         }
     }
 
-    func fetchUser(by id: String,
-                   completion: ((User) -> Void)?) {
+    func fetchUser(by id: String, completion: ((User) -> Void)?) {
         FirebaseService.user(of: id) { dto in
             completion?(User(userDTO: dto))
         }
     }
 
-    func updateContinuityDay(by id: String,
-                             completion: ((UserDTO) -> Void)?) {
+    func updateContinuityDay(by id: String, completion: ((UserDTO) -> Void)?) {
         FirebaseService.updateContinuityDay(of: id) { dto in
             completion?(dto)
         }
     }
 
-    func updateContinuityDayByAuth(by id: String,
-                                   completion: (() -> Void)?) {
+    func updateContinuityDayByAuth(by id: String, completion: (() -> Void)?) {
         FirebaseService.updateContinuityDayByAuth(of: id) {
             completion?()
         }
@@ -66,17 +52,13 @@ extension RoutinusRepository: UserRepository {
         return UserDefaults.standard.integer(forKey: RoutinusRepository.themeStyleKey)
     }
 
-    func updateUsername(of id: String,
-                        name: String,
-                        completion: (() -> Void)?) {
-        FirebaseService.updateUsername(of: id,
-                                       name: name) {
+    func updateUsername(of id: String, name: String, completion: (() -> Void)?) {
+        FirebaseService.updateUsername(of: id, name: name) {
             completion?()
         }
     }
 
     func updateThemeStyle(_ style: Int) {
-        UserDefaults.standard.set(style,
-                                  forKey: RoutinusRepository.themeStyleKey)
+        UserDefaults.standard.set(style, forKey: RoutinusRepository.themeStyleKey)
     }
 }
